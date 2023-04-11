1722ae97f2a2aff4cc06bbda68af3761fb9ed03a
Melanie S <melanie.sapiandante@gmail.com>
2023-04-11 16:15:43 -0400

    added to index.js file

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+//do I need fs for the challenge? I think so because it's a libarary... will follow up.  
+//how did we install or link inquirer and fs? i know the above and below code starts it. how to finish? 
+const fs = require('fs'); 
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -11,3 +15,31 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+​
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
