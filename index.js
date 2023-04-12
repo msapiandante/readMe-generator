e603e419ee907c5733777c30da59d172af86f4ec
Melanie S <melanie.sapiandante@gmail.com>
2023-04-11 21:15:04 -0400

    added to index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 const inquirer = require("inquirer");
 //do I need fs for the challenge? I think so because it's a libarary... will follow up.  
 //how did we install or link inquirer and fs? i know the above and below code starts it. how to finish? 
-const fs = require('fs'); 
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
@@ -26,42 +26,55 @@ inquirer
     {
       type: 'input',
       title: 'description',
-      message: 'Enter project description.'
-       "Table of Contents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"],
+      message: 'Write project description.'
+
     },
     {
-        type: 'input',
-        title: 'Table of Contents',
-        message: 'What is your project title?',
-      },
-      {
-        type: 'input',
-        title: 'title',
-        message: 'What is your project title?',
-      },
-      {
-        type: 'input',
-        title: 'title',
-        message: 'What is your project title?',
-      },
-      {
-        type: 'input',
-        title: 'title',
-        message: 'What is your project title?',
-      },
-      {
-        type: 'input',
-        title: 'title',
-        message: 'What is your project title?',
-      },
-    
-    
-    'email', 'phone', 'telekinesis'],
+      //should this be the "list" type? 
+      type: 'input',
+      title: 'Table of Contents',
+      message: 'List table of contents',
+    },
+    {
+      type: 'input',
+      title: 'Installation',
+      message: 'Enter notes on installation here',
+    },
+    {
+      type: 'input',
+      title: 'Usage',
+      message: 'Enter notes on usage here',
+    },
+    {
+      type: 'input',
+      title: 'License',
+      message: 'Enter license information here',
+    },
+    {
+      type: 'input',
+      title: 'Contributing',
+      message: 'How can other developers contribute to this project?',
+    },
+    {
+    type: 'input',
+    title: 'Contributing',
+    message: 'How can other developers contribute to this project?',
+    },
+    {
+    type: 'input',
+    title: 'Tests',
+    message: 'Enter any test information here',
+    },
+    {
+    type: 'input',
+    title: 'Questions',
+    message: 'Write questions section here',
     },
   ])
+
   .then((data) => {
     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-​
+
     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
       err ? console.log(err) : console.log('Success!')
     );
